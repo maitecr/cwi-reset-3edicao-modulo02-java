ce54b48e2e7efc2fe1649f0a25cc7ddb9bcab7df
Maitê <81782090+maitecr@users.noreply.github.com>
2021-08-20 16:08:50 -0300

    Create AlunoResetLeituraTeclado.java

---
+++ AlunoResetLeituraTeclado.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AlunoResetLeituraTeclado {
+
+	public static void main(String[] args) {
+		Scanner teclado =  new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		String nome = teclado.nextLine();
+		
+		System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + nome);
+		
+
+	}
+
+}
